0efae8fd04bc1a07bcf236ce665dc9fe116c25dd
Marco Torres <marcotorres258@outlook.com>
2022-11-29 11:19:25 -0600

    Check

--- 2D_SImulation/agents/Destination.py
+++ 2D_SImulation/agents/Destination.py
@@ -17,23 +17,9 @@ class Destination(Agent):
     def clean_cars(self):
         """Remove cars that are in destiny"""
         # Get destination agents
-        agents = self.get_cell_agents(self.pos)
+        agents = self.model.get_cell_agents(self.pos)
 
         # Remove cars
         for a in agents:
             if isinstance(a, Car):
                 self.model.grid.remove_agent(a)
-
-
-    def get_cell_agents(self, position):
-        """Get list of agents of a cell."""
-        # Get neighbors including center cell
-        neighbors = self.model.grid.get_neighbors(position, False, True)
-
-        # Get agents in position
-        agents = []
-        for n in neighbors:
-            if n.pos == position:
-                agents.append(n)
-
-        return agents
